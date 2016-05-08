c20f41c68b48a85dacb0fd2bd4a8f514cc62a148
andela-ydaniju <yusuf.daniju@andela.com>
2016-05-08 18:54:35 +0100

    ignore coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 abort("The Rails environment is running in production mode!") if
